b4fe8817a26f3eeb46f66529bb0fb95e4489c643
honnage <hon_da-1998@hotmail.com>
2023-05-11 16:10:00 +0700

    util path

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -2,16 +2,17 @@ const express = require('express')
 const router = express.Router()
 
 const path = require('path')
+const rootDit = require('../util/path')
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
-})
+    res.sendFile(path.join(rootDit, 'views', 'add-product.html'))
+  })
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
     console.log(req.body)
-    res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'))
+    res.redirect('/')
 })
 
 module.exports = router
