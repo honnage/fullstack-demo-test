0664cba52caae02da55a965fec3edf6a2bbd4d0f
honnage <hon_da-1998@hotmail.com>
2023-05-11 15:22:50 +0700

    update routes

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,13 @@ const shopRouter = require('./routes/shop')
 const app = express()
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(adminRouter)
+
+app.use('/admin',adminRouter)
 app.use(shopRouter)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3033)
 console.log('Server is running port 3033')
