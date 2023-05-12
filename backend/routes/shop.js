b8f714ab868202cb9541ca5e3fbc733724927a0e
honnage <hon_da-1998@hotmail.com>
2023-05-12 09:44:02 +0700

    pug

--- backend/routes/shop.js
+++ backend/routes/shop.js
@@ -7,8 +7,9 @@ const rootDir = require('../util/path')
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products)
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+  const products = adminData.products
+  res.render('shop', {prods: products, docTitle: 'Shop'})
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
 
 module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ const trackerRouter = require('./routes/tracker')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -16,7 +19,8 @@ app.use(shopRouter)
 app.use(trackerRouter)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 })
 
 app.listen(3033)
